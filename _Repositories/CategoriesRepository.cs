1383c06d2c465c2b8171e3921c3d172ac598dcd2
Juan Jose Gomez Bravo <juangomez130703@gmail.com>
2024-04-19 23:21:09 -0500

    Categories Repository - Search

---
+++ _Repositories/CategoriesRepository.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+using Microsoft.Data;
+using System.Data;
+using supermarkett_mvp.Models;
+
+namespace supermarkett_mvp._Repositories
+{
+    internal class CategoriesRepository : BaseRepository, ICategoriesRepository
+    {
+        public CategoriesRepository(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+
+        public void Add(CategoriesModel categoriesModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Edit(CategoriesModel categoriesModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<CategoriesModel> GetAll()
+        {
+            //throw new NotImplementedException();
+            var categoriesList = new List<CategoriesModel>();
+            using (var connection = new SqlConnection(connectionString))
+            using (var command = new SqlCommand())
+            {
+                connection.Open();
+                command.Connection = connection;
+                command.CommandText = "SELECT * FROM Categories ORDER BY Categorie_Id DESC";
+                using (var reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        var categoriesModel = new CategoriesModel();
+                        categoriesModel.Id = (int)reader["Categorie_Id"];
+                        categoriesModel.Name = reader["Categorie_Name"].ToString();
+                        categoriesModel.Observation = reader["Categorie_Observation"].ToString();
+                        categoriesList.Add(categoriesModel);
+                    }
+                }
+            }
+            return categoriesList;
+        }
+
+        public IEnumerable<CategoriesModel> GetByValue(string value)
+        {
+            //throw new NotImplementedException();
+            var categoriesList = new List<CategoriesModel>();
+            int categorieId = int.TryParse(value, out _) ? Convert.ToInt32(value) : 0;
+            string categorieName = value;
+            using (var connection = new SqlConnection(connectionString))
+            using (var command = new SqlCommand())
+            {
+                connection.Open();
+                command.Connection = connection;
+                command.CommandText = @"SELECT * FROM Categories 
+                                        WHERE Categorie_Id=@id or Categorie_Name LIKE @name+ '%'
+                                        ORDER BY Categorie_Id DESC";
+                command.Parameters.Add("@id", SqlDbType.Int).Value = categorieId;
+                command.Parameters.Add("@name", SqlDbType.NVarChar).Value = categorieName;
+                using (var reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        var categoriesModel = new CategoriesModel();
+                        categoriesModel.Id = (int)reader["Categorie_Id"];
+                        categoriesModel.Name = reader["Categorie_Name"].ToString();
+                        categoriesModel.Observation = reader["Categorie_Observation"].ToString();
+                        categoriesList.Add(categoriesModel);
+                    }
+                }
+            }
+
+            return categoriesList;
+        }
+    }
+}
