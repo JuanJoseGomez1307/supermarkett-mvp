b85b8064fa3661625b6587dc81d34690a973fca5
Juan Jose Gomez Bravo <juangomez130703@gmail.com>
2024-04-19 23:01:22 -0500

    Categories Presenter

---
+++ Presenters/CategoriesPresenter.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using supermarkett_mvp.Views;
+using supermarkett_mvp.Models;
+
+namespace supermarkett_mvp.Presenters
+{
+    internal class CategoriesPresenter
+    {
+        private ICategoriesView view;
+        private ICategoriesRepository repository;
+        private BindingSource categoriesBindingSource;
+        private IEnumerable<CategoriesModel> categoriesList;
+
+        public CategoriesPresenter(ICategoriesView view, ICategoriesRepository repository)
+        {
+            this.categoriesBindingSource = new BindingSource();
+
+            this.view = view;
+            this.repository = repository;
+
+            this.view.SearchEvent += SearchCategories;
+            this.view.AddNewEvent += AddNewCategories;
+            this.view.EditEvent += LoadSelectCategoriesToEdit;
+            this.view.DeleteEvent += DeleteSeletedCategories;
+            this.view.SaveEvent += SaveCategories;
+            this.view.CancelEvent += CancelAction;
+
+            this.view.SetCategoriesListBildingSource(categoriesBindingSource);
+
+            loadAllCategoriesList();
+
+            this.view.Show();
+
+        }
+
+        private void loadAllCategoriesList()
+        {
+            //throw new NotImplementedException();
+            categoriesList = repository.GetAll();
+            categoriesBindingSource.DataSource = categoriesList;
+        }
+
+        private void CancelAction(object? sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void SaveCategories(object? sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void DeleteSeletedCategories(object? sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void LoadSelectCategoriesToEdit(object? sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void AddNewCategories(object? sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void SearchCategories(object? sender, EventArgs e)
+        {
+            //throw new NotImplementedException();
+            bool emptyValue = string.IsNullOrEmpty(this.view.SearchValue);
+
+            if (emptyValue == false)
+            {
+                categoriesList = repository.GetByValue(this.view.SearchValue);
+            }
+            else
+            {
+                categoriesList = repository.GetAll();
+            }
+            categoriesBindingSource.DataSource = categoriesList;
+        }
+    }
+}
